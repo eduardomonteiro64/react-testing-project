d7c16ef09ffd53e26e3505d9e48f17028de3974c
Eduardo do Carmo <eduardomonteiro64@icloud.com>
2022-08-01 10:31:12 -0300

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { render, screen } from '@testing-library/react'
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom"
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe("App component", () => {
+    test("codesweetly test heading", () => {
+        render(<App />);
+        expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(/CodeSweetly Test/i);
+    });
+
+    test("a codesweetly project heading", () => {
+        render(<App />);
+
+        const button = screen.getByRole("button", { name: "Update Heading" });
+
+        userEvent.click(button);
+
+        expect(screen.getByRole("heading")).toHaveTextContent(/A CodeSweetly Project/i);
+    });
 });
